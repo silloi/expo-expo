5c8e727c183e7ac0e53b806c263b89682021fe7e
silloi <silloi1993@gmail.com>
2020-10-24 19:28:35 +0900

    feat: add screen features

--- App.tsx
+++ App.tsx
@@ -1,65 +1,20 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator, StackHeaderProps } from 'react-navigation-stack';
-import { createDrawerNavigator, DrawerActions, DrawerContentComponentProps, DrawerItems } from 'react-navigation-drawer';
-import { View, Text } from 'react-native';
-import { Icon, Header, Avatar, Badge } from 'react-native-elements';
-import { StackNavigationProp } from 'react-navigation-stack/lib/typescript/src/vendor/types';
-import { ScrollView } from 'react-native-gesture-handler';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
-interface HamburgerMenuProps {
-  navigation: StackNavigationProp;
-}
-const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
-  navigation
-}: HamburgerMenuProps) => {
-  return (
-    <Icon
-      color="#fff"
-      name="menu"
-      onPress={() => {
-        navigation.dispatch(
-          DrawerActions.openDrawer()
-        )
-      }}
-    />
-  );
-}
 
-const MyHeader: React.FC<StackHeaderProps> = ({
-  navigation,
-  styleInterpolator
-}: StackHeaderProps) => {
-  return (
-    <Header
-      leftComponent={<HamburgerMenu navigation={navigation} />}
-      centerComponent={{ text: 'expo-expo', style:{ color: '#fff' }}}
-    />
-  );
-}
-
-const Screen1: React.FC = () => {
-  return (
-    <View style={{flex:1, alignItems:'center', justifyContent: 'center'}}>
-      <Text>Screen1</Text>
-    </View>
-  );
-}
-
-const Screen2: React.FC = () => {
-  return (
-    <View style={{flex:1, alignItems:'center', justifyContent: 'center'}}>
-      <Text>Screen2</Text>
-    </View>
-  );
-}
+import MyHeader from './components/header/MyHeader';
+import MyDrawerContent from './components/header/MyDrawerContent';
+import MainScreen from './components/screens/MainScreen';
+import SettingsScreen from './components/screens/SettingsScreen';
 
 const Stack1 = createStackNavigator(
   {
     Stack1: {
-      screen: Screen1,
+      screen: MainScreen,
       navigationOptions: {
-        title: "Screen1",
+        title: "Main",
         header: MyHeader
       }
     }
@@ -69,49 +24,22 @@ const Stack1 = createStackNavigator(
 const Stack2 = createStackNavigator(
   {
     Stack2: {
-      screen: Screen2,
+      screen: SettingsScreen,
       navigationOptions: {
-        title: "Screen2",
+        title: "Settings",
         header: MyHeader
       }
     }
   }
 )
 
-const MyDrawerContent: React.FC<DrawerContentComponentProps> = 
-  (props: DrawerContentComponentProps) => {
-    const icon = "https://pbs.twimg.com/profile_images/1256226829674913799/upEPbuy__400x400.png";
-    return (
-      <ScrollView>
-        <View style={{height:120, marginTop:24, backgroundColor:'gray'}}>
-          <View style={{height:80, width:80, marginTop:10, marginLeft:10}}>
-            <Avatar
-              rounded
-              size="large"
-              source={{uri:icon}}
-            />
-            <Badge
-              status="success"
-              value={"54"}
-              containerStyle={{ position:'absolute', top:5, right:0 }}
-            />
-          </View>
-          <View style={{marginLeft:10}}>
-            <Text style={{color:'white'}}>@silloi</Text>
-          </View>
-        </View>
-        <DrawerItems {...props} />
-      </ScrollView>
-    )
-}
-
 const Drawer = createDrawerNavigator(
   {
-    Screen1: {screen: Stack1},
-    Screen2: {screen: Stack2},
+    Main: {screen: Stack1},
+    Settings: {screen: Stack2},
   },
   {
-    initialRouteName: 'Screen1',
+    initialRouteName: 'Main',
     contentComponent: MyDrawerContent
   }
 );
