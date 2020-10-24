35bf3334d9bd8c9468dcd3b47dfd2b6b61f209cb
silloi <silloi1993@gmail.com>
2020-10-24 15:23:09 +0900

    feat: add react-native-elements components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React, { useState, useEffect } from 'react';
+import { View, ActivityIndicator, Dimensions } from 'react-native';
+import { Avatar, Badge, Button, ButtonGroup, Card, CheckBox, Divider, Image, Input } from 'react-native-elements';
 
 const App: React.FC = () => {
+  const [text1, setText1] = useState<string>('');
+  const  [selectedIdx, setSelectedIdx] = useState<number>(0);
+  const  buttons = ['What', 'the', 'Fuck'];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1, paddingVertical: 80}}>
+      <Card>
+        <Card.Title>expo-expo</Card.Title>
+        <View style={{width:80, marginLeft: 10}}>
+          <Avatar
+            containerStyle={{backgroundColor:'gray'}}
+            rounded
+            size="large"
+            icon={{ name: 'home' }}
+          />
+          <Badge
+            status="success"
+            value={buttons[selectedIdx]}
+            containerStyle={{ position: 'absolute', top: -5, right: -5 }}
+          />
+        </View>
+        <Divider style={{
+          backgroundColor: 'gray',
+          margin:10,
+          height:1,
+          }}
+        />
+        <ButtonGroup
+          onPress={(i)=>{
+            setSelectedIdx(i)
+          }}
+          selectedIndex={selectedIdx}
+          buttons={buttons}
+          containerStyle={{height: 40}}
+        />
+      </Card>
     </View>
   );
 }
