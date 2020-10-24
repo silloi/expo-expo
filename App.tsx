a51e1f149851b66cbebfc970199302a538c27c70
silloi <silloi1993@gmail.com>
2020-10-24 15:01:19 +0900

    feat: rewrite React Component Style to React Functional Component (React.FC) style

--- App.tsx
+++ App.tsx
@@ -2,20 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -24,3 +10,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default App;
